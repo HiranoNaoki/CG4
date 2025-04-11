770852a8791549618480e16d8311e59e3b0228d8
HiranoNaoki <146906525+HiranoNaoki@users.noreply.github.com>
2025-04-11 11:02:50 +0900

    kiso

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,7 +1,42 @@
 #include <Windows.h>
+#include <KamataEngine.h>
+#include "GameScene.h"
+
+using namespace KamataEngine;
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	
+	KamataEngine::Initialize(L"LE3D_13_ヒラノ_ナオキ_CG4");
+
+
+	DirectXCommon* dxcommon = DirectXCommon::GetInstance();
+
+	GameScene* gameScene = new GameScene();
+
+	gameScene->Initialize();
+
+	while (true) {
+		if (KamataEngine::Update()) {
+			break;
+		}
+
+		gameScene->Update();
+
+		dxcommon->PreDraw();
+
+		gameScene->Draw();
+
+	    dxcommon->PostDraw();
+	}
+
+
+	KamataEngine::Finalize();
+
+	
+	delete gameScene;
+
+	gameScene = nullptr;
+
 	return 0;
 }
