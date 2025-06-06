6f5f788775707ab424cd54b244c1946e946fd281
HiranoNaoki <146906525+HiranoNaoki@users.noreply.github.com>
2025-06-06 10:49:08 +0900

    toti

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -75,13 +75,13 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
 	// 頂点シェーダの読み込みとコンパイル
 	Shader vs;
-	vs.Load(L"Resources/shaders/TestVS.hlsl", "vs_5_0");
-	assert(vs.GetBlob() != nullptr);
+	vs.LoadDxc(L"Resources/shaders/TestVS.hlsl", L"vs_6_0");
+	assert(vs.GetDxcBlob() != nullptr);
 
 	// ピクセルシェーダの読み込みとコンパイル
 	Shader ps;
-	ps.Load(L"Resources/shaders/TestPS.hlsl", "ps_5_0");
-	assert(ps.GetBlob() != nullptr);
+	ps.LoadDxc(L"Resources/shaders/TestPS.hlsl", L"ps_6_0");
+	assert(ps.GetDxcBlob() != nullptr);
 
 	
 	//PSO(PipelineStateObject)の生成
