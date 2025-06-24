e48e300dc89efe6de48d2b9bef4aa731e22ccbae
HiranoNaoki <146906525+HiranoNaoki@users.noreply.github.com>
2025-06-24 10:43:08 +0900

    owata07

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -6,6 +6,7 @@
 #include "RootSignature.h"
 #include "PipelineState.h"
 #include "VertexBuffer.h"
+#include "IndexBuffer.h"
 //#include <d3dcompiler.h>
 
 using namespace KamataEngine;
@@ -59,26 +60,49 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
 	SetupPipelineState(pipelineState, rs, vs, ps);
 
+	struct VertexDate {
+		Vector4 position;
+	};
 
+	VertexDate vertices[] = {
+	    {-1.0f,  1.0f, 0.0f, 1.0f}, // 左上
+	    { 1.0f,  1.0f, 0.0f, 1.0f}, // 右上
+	    { 1.0f, -1.0f, 0.0f, 1.0f}, // 右下
+	    {-1.0f, -1.0f, 0.0f, 1.0f}, // 左下
+	};
 
 
 	//VertexResourceの生成
 	
-	VertexBuffer vd;
-	vd.Create(sizeof(Vector4) * 3, sizeof(Vector4));
+	VertexBuffer vb;
+	//vd.Create(sizeof(Vector4) * 3, sizeof(Vector4));
+	vb.Create(sizeof(vertices), sizeof(vertices[0]));
 
 
-	//頂点リソースにデータを書き込む
-	Vector4* vertexData = nullptr;
-	vd.Get()->Map(0, nullptr, reinterpret_cast<void**>(&vertexData));
-	vertexData[0] = {-0.5f, -0.5f, 0.0f, 1.0f};//左下
-	vertexData[1] = {0.0f, 0.5f, 0.0f, 1.0f}; //上
-	vertexData[2] = {0.5f, -0.5f, 0.0f, 1.0f}; // 右下
+	VertexDate* pGpuVertices = nullptr;
+	vb.Get()->Map(0, nullptr, reinterpret_cast<void**>(&pGpuVertices));
+	for (int i = 0; i < _countof(vertices); ++i) {
+		pGpuVertices[i] = vertices[i];
+	}
+
 	//頂点リソースのマップを解除する
 	//vertexResource->Unmap(0, nullptr);
 
+	uint16_t indices[] = {
+	    0, 1, 2, 
+	    2, 3, 0, 
+	};
 
+	IndexBuffer ib;
+	ib.Create(sizeof(indices), sizeof(indices[0]));
 
+	// 頂点リソースにデータを書き込む
+	uint16_t* pGpuIndices = nullptr;
+	ib.Get()->Map(0, nullptr, reinterpret_cast<void**>(&pGpuIndices));
+
+	for (int i = 0; i < _countof(indices); ++i) {
+		pGpuIndices[i] = indices[i];
+	}
 	
 
 	while (true) {
@@ -93,11 +117,13 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 		//コマンドを詰む
 		commandList->SetGraphicsRootSignature(rs.Get());//RootSignatureの設定
 		commandList->SetPipelineState(pipelineState.Get());//PSOの設定をする
-		commandList->IASetVertexBuffers(0, 1, vd.GetView());//VBVの設定をする
+		commandList->IASetVertexBuffers(0, 1, vb.GetView());//VBVの設定をする
+		commandList->IASetIndexBuffer(ib.GetView());
 		//トポロジの設定
 		commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 		//頂点数、インデックス数、インデックスの開始位置、インデックスのオフセット
-		commandList->DrawInstanced(3, 1, 0, 0);
+		//commandList->DrawInstanced(3, 1, 0, 0);
+		commandList->DrawIndexedInstanced(_countof(indices), 1, 0, 0, 0);
 
 		gameScene->Draw();
 
