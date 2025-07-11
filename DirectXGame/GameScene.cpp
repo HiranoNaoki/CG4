7f727a43ecc6822700eb3bce014764a572c9db7c
HiranoNaoki <146906525+HiranoNaoki@users.noreply.github.com>
2025-07-11 11:08:36 +0900

    05-02owaq

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -1,15 +1,34 @@
 #include "GameScene.h"
+#include <cmath>
 
 using namespace KamataEngine;
 
 void GameScene::Initialize() {
 
+	textureHandle = TextureManager::Load("start.png");
+
+	sprite_ = Sprite::Create(textureHandle, {0, 0});
 }
 
-void GameScene::Update() {
+void GameScene::Update() { frame++;
 
+float y = 45 * sin(frame * 0.05f);
+
+sprite_->SetPosition({0.0f, y});
 }
 
-void GameScene::Draw() {
+void GameScene::Draw() { 
+	
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+	Sprite::PreDraw(dxCommon->GetCommandList());
 
+
+	//if (frame %60 >=30) {
+		sprite_->Draw();
+	//}
+	
+	Sprite::PostDraw(); 
 }
+GameScene::GameScene() {}
+
+GameScene::~GameScene() {}
--- DirectXGame/GameScene.h
+++ DirectXGame/GameScene.h
@@ -3,10 +3,19 @@
 
 class GameScene {
 public:
+	GameScene();
+	~GameScene();
 
 	void Initialize();
 
 	void Update();
 
 	void Draw();
+
+private:
+	uint32_t textureHandle = 0;
+
+	KamataEngine::Sprite* sprite_ = nullptr;
+
+	int frame = 0;
 };
